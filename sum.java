98ef062ec31f583dd08a997a3b4fd6af6bfe4511
Ilakkanan <ilakkananwork@gmail.com>
2023-01-02 14:50:37 +0530

    merge conflict resolved

--- sum.java
+++ sum.java
@@ -2,11 +2,8 @@ class sum{
     public static void main(String args[]){
         int a=3;
         int b=8;
-<<<<<<< HEAD
         int d=6;
-=======
         int c=6;
->>>>>>> d273d6d433ca8105de1a195a039bc82b7d40c6e4
         System.out.println(a+b);
         System.out.println(a-b);
     }
